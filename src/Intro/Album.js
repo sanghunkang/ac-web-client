7a6615945415562a2e39c28156cdd89d298cc92f
sanghunkang <sanghunkang.dev@gmail.com>
2019-11-10 14:34:10 +0900

    resolving proxy5

--- src/Intro/Album.js
+++ src/Intro/Album.js
@@ -205,7 +205,6 @@ class Home extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
