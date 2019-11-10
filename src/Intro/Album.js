31765578b7bdf7b1decd1c6bb97eddb4c286769a
sanghunkang <sanghunkang.dev@gmail.com>
2019-11-10 14:25:36 +0900

    resolving proxy4

--- src/Intro/Album.js
+++ src/Intro/Album.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
@@ -88,6 +88,7 @@ export default function Album(props) {
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
               Adative Cram을 사용하는 시간은 당신의 미래를 바꿀 수 있습니다.
             </Typography>
+            <Home />
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <p>
@@ -159,4 +160,78 @@ export default function Album(props) {
       {/* End footer */}
     </React.Fragment>
   );
+}
+
+
+const AnotherPage = () => <h1>Another Page</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
+class Home extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.msg }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/.netlify/functions/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/.netlify/functions/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ responseToPost: body });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>{this.state.response}</p>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <strong>Post to Server:</strong>
+            </p>
+            <input
+              type="text"
+              value={this.state.post}
+              onChange={e => this.setState({ post: e.target.value })}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.responseToPost}</p>
+        </header>
+      </div>
+    );
+  }
 }
